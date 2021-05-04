a84ec7da4da21cafa7687b9795ab5c73735099db
anthonyapicella <73201390+anthonyapicella@user.noreply.github.com>
2021-05-04 09:39:46 -0700

    Navbar finished

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,70 +1,72 @@
 import React from 'react';
+// font awesome
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const Navbar = () => {
 	return (
 		<nav className='navbar navbar-expand-lg navbar-dark bg-dark p-3'>
 			{/* <div className='container'> */}
-				<a className='navbar-brand ml-1' href='#'>
-					Anthony Apicella
-				</a>
-				<button
-					className='navbar-toggler'
-					type='button'
-					data-toggle='collapse'
-					data-target='#navbarNav'
-					aria-controls='navbarNav'
-					aria-expanded='false'
-					aria-label='Toggle navigation'
-				>
-					<span className='navbar-toggler-icon'></span>
-				</button>
-				<div className='collapse navbar-collapse' id='navbarNav'>
-					<ul className='navbar-nav mx-auto'>
-						<li className='nav-item active'>
-							<a className='nav-link' href='#'>
-								Home <span className='sr-only'>(current)</span>
+			<a className='navbar-brand ml-1' href='#'>
+				Anthony Apicella
+			</a>
+			<button
+				className='navbar-toggler'
+				type='button'
+				data-toggle='collapse'
+				data-target='#navbarNav'
+				aria-controls='navbarNav'
+				aria-expanded='false'
+				aria-label='Toggle navigation'
+			>
+				<span className='navbar-toggler-icon'></span>
+			</button>
+			<div className='collapse navbar-collapse' id='navbarNav'>
+				<ul className='navbar-nav mx-auto'>
+					<li className='nav-item active'>
+						<a className='nav-link' href='#'>
+							Home <span className='sr-only'>(current)</span>
+						</a>
+					</li>
+					<li className='nav-item'>
+						<a className='nav-link' href='#'>
+							About Me
+						</a>
+					</li>
+					<li className='nav-item dropdown'>
+						<a
+							className='nav-link dropdown-toggle'
+							data-toggle='dropdown'
+							href='#'
+							role='button'
+							aria-haspopup='true'
+							aria-expanded='false'
+						>
+							My Work
+						</a>
+						<div className='dropdown-menu'>
+							<a className='dropdown-item' href='#'>
+								Project #1
 							</a>
-						</li>
-						<li className='nav-item'>
-							<a className='nav-link' href='#'>
-								About Me
+							<a className='dropdown-item' href='#'>
+								Project #2
 							</a>
-						</li>
-						<li className='nav-item dropdown'>
-							<a
-								className='nav-link dropdown-toggle'
-								data-toggle='dropdown'
-								href='#'
-								role='button'
-								aria-haspopup='true'
-								aria-expanded='false'
-							>
-								My Work
+							<a className='dropdown-item' href='#'>
+								Project #3
 							</a>
-								<div className='dropdown-menu'>
-									<a className='dropdown-item' href='#'>
-										Action
-									</a>
-									<a className='dropdown-item' href='#'>
-										Another action
-									</a>
-									<a className='dropdown-item' href='#'>
-										Something else here
-									</a>
-									<div className='dropdown-divider'></div>
-									<a className='dropdown-item' href='#'>
-										Separated link
-									</a>
-								</div>
-						</li>
-						
-						<li className='nav-item'>
-							<a className='nav-link' href='#'>
-								Resume
+							<div className='dropdown-divider'></div>
+							<a className='dropdown-item' href='#'>
+								Separated link
 							</a>
-						</li>
-					</ul>
-				</div>
+						</div>
+					</li>
+
+					<li className='nav-item'>
+						<a className='nav-link' href='#'>
+							Resume
+						</a>
+					</li>
+				</ul>
+			</div>
 			{/* </div> */}
 		</nav>
 	);
