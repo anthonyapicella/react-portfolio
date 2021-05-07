087ea84b2d80e1dbd88691e5654784b6714b4dec
anthonyapicella <73201390+anthonyapicella@user.noreply.github.com>
2021-05-06 22:12:41 -0700

    Updates

---
+++ precache-manifest.e7da8a93ee5dcd7f5c0de6b80d98aab0.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efe27700c1d937e38efed0ff53f08a3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51444a57b2057f5bd0e9",
+    "url": "/static/css/2.3e64c57b.chunk.css"
+  },
+  {
+    "revision": "0e8d9ba5b75a0a79ab45",
+    "url": "/static/css/main.bdcc2272.chunk.css"
+  },
+  {
+    "revision": "51444a57b2057f5bd0e9",
+    "url": "/static/js/2.d88a986c.chunk.js"
+  },
+  {
+    "revision": "ed186a0419b9c0342b0832cac3adaa4f",
+    "url": "/static/js/2.d88a986c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1fa2beb6c86a187636e",
+    "url": "/static/js/3.551c8a5c.chunk.js"
+  },
+  {
+    "revision": "0e8d9ba5b75a0a79ab45",
+    "url": "/static/js/main.11d1e55e.chunk.js"
+  },
+  {
+    "revision": "5526f739107106eb3b7e",
+    "url": "/static/js/runtime-main.8cc252b1.js"
+  },
+  {
+    "revision": "2e4353135d532ab37f91cd93cbe204cb",
+    "url": "/static/media/12_MYSQL.2e435313.gif"
+  },
+  {
+    "revision": "5b33ce658236e87d19f6c196fdcafe1a",
+    "url": "/static/media/HEADSHOT.5b33ce65.png"
+  },
+  {
+    "revision": "8d32b20391f500c55d759ff560026278",
+    "url": "/static/media/TPG1.8d32b203.png"
+  },
+  {
+    "revision": "5482160d70357998150b7377ffbf5243",
+    "url": "/static/media/Title.5482160d.png"
+  },
+  {
+    "revision": "b18f8daa0cb3bcf4a7ef871ac764dda5",
+    "url": "/static/media/VTPscreen.b18f8daa.png"
+  },
+  {
+    "revision": "4566c903e15887e52ddcfb2dafed5b2d",
+    "url": "/static/media/Weather.4566c903.gif"
+  },
+  {
+    "revision": "b6189b758e4c83d70a296798c4798df3",
+    "url": "/static/media/XPpedia.b6189b75.gif"
+  },
+  {
+    "revision": "65f2a8a9020eb64b398c79120d75fb67",
+    "url": "/static/media/anthonyApicellaResume.65f2a8a9.pdf"
+  },
+  {
+    "revision": "c924b974e1428c202ad83d7efcfe827d",
+    "url": "/static/media/backend1.c924b974.png"
+  },
+  {
+    "revision": "5aa0ba60ca648e1038ad2fb1b387d015",
+    "url": "/static/media/codeBackgroung.5aa0ba60.jpeg"
+  }
+]);
